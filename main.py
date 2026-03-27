bfc29f7b980d6766e472ac3c6c53f822912c2bc1
nandika-k <nkarnik5@outlook.com>
2026-03-27 17:42:08 -0400

    router for dashboard added to main

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from fastapi.staticfiles import StaticFiles
 from routers.auth import router as auth_router
 from routers.clients import router as clients_router
 from routers.coaches import router as coaches_router
+from routers.dashboard import router as dashboard_router
 from routers.exercises import router as exercises_router
 from routers.workouts import router as workouts_router
 
@@ -27,6 +28,7 @@ app.include_router(clients_router)
 app.include_router(workouts_router)
 app.include_router(coaches_router)
 app.include_router(exercises_router)
+app.include_router(dashboard_router)
 
 # serve everything inside the local ./uploads folder at the /uploads URL path
 os.makedirs("uploads", exist_ok=True)
