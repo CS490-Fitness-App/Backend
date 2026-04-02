c8661cb7d892db616c77425a49e8676525a4448f
nandika-k <nkarnik5@outlook.com>
2026-04-02 17:31:55 -0400

    fk strings lowercase

--- models/workout.py
+++ models/workout.py
@@ -72,8 +72,8 @@ class SetResult(Base):
     __tablename__ = 'set_results'
 
     set_results_id = Column(Integer, primary_key=True, autoincrement=True)
-    workout_log_id = Column(Integer, ForeignKey('Workout_Logs.workout_log_id', ondelete='CASCADE'), nullable=False)
-    exercise_id    = Column(Integer, ForeignKey('Exercises.exercise_id', ondelete='CASCADE'), nullable=True)
+    workout_log_id = Column(Integer, ForeignKey('workout_logs.workout_log_id', ondelete='CASCADE'), nullable=False)
+    exercise_id    = Column(Integer, ForeignKey('exercises.exercise_id', ondelete='CASCADE'), nullable=True)
     actual_weight  = Column(Numeric(8, 2))
     actual_value   = Column(Numeric(8, 2))
     created_at     = Column(DateTime(timezone=True), nullable=False, default=_now)
