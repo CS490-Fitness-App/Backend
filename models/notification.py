c3cd5512573053b44f24208a521101b3db65ee52
nandika-k <nkarnik5@outlook.com>
2026-04-02 16:47:40 -0400

    models updated to match railway implementation

--- models/notification.py
+++ models/notification.py
@@ -12,10 +12,10 @@ def _now():
 
 
 class Notification(Base):
-    __tablename__ = 'Notifications'
+    __tablename__ = 'notifications'
 
     notification_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id         = Column(Integer, ForeignKey('Users.user_id', ondelete='CASCADE'), nullable=False)
+    user_id         = Column(Integer, ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False)
     message         = Column(Text, nullable=False)
     created_at      = Column(DateTime(timezone=True), nullable=False, default=_now)
     last_updated    = Column(DateTime(timezone=True), nullable=False, default=_now)
