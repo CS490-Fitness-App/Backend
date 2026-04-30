ab5ee9c211d83ae040102fe56a1927b3446e6081
Simon Ogorek <47757232+Simon-Ogorek@users.noreply.github.com>
2026-04-30 10:06:16 -0400

    Hotfix for a unit test

--- schemas/review.py
+++ schemas/review.py
@@ -3,7 +3,7 @@
 
 from datetime import datetime
 from typing import Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class ReviewIn(BaseModel):
